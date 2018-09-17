8153c4120e8c1d41a6603d537c079a08d1acad76
'root' <'root@qq.com'>
2018-09-17 12:36:08 +0800

    项目的初始化

---
+++ src/cn/mldn/test/TestOne.java
@@ -0,0 +1,7 @@
+package cn.mldn.test;
+
+public class TestOne {
+	public static void main(String args[]) {
+		System.out.println("Test One");
+	}
+}
---
+++ src/cn/mldn/vo/Emp.java
@@ -0,0 +1,9 @@
+package cn.mldn.vo;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Emp implements Serializable{
+	private Integer empno;
+	private String ename;
+}
